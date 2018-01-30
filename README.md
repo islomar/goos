09dbe4d1fde17e849e84281829419029a9394a54
Isidro López <islomar@gmail.com>
2018-01-30 11:52:48 +0100

    First end2end test for the walking skeleton.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# goos
-Notes and code example from the book "Growing Object-Oriented Software, Guided by Tests"
+# GOOS notes
+Notes and code related with the book "Growing Object-Oriented Software, Guided by Tests".
+
+## Links
+* http://www.growing-object-oriented-software.com/
+* http://www.growing-object-oriented-software.com/code.html
+* https://github.com/npryce/goos-code-examples
+
+## Pending
+* https://gojko.net/2010/03/28/brian-marick-mocks-lead-to-better-design-faster/
---
+++ src/test/java/com/goos/auctionsniper/AuctionSniperEndToEndTest.java
@@ -0,0 +1,30 @@
+package com.goos.auctionsniper;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+public class AuctionSniperEndToEndTest {
+    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final FakeAuctionServer auction2 = new FakeAuctionServer("item-65432");
+
+    private final ApplicationRunner application = new ApplicationRunner();
+
+    @Test
+    public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+        auction.startSellingItem();
+        application.startBiddingIn(auction);
+        auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
+        auction.announceClosed();
+        application.hasShownSniperHasLostAuction(auction, 0, 0);
+    }
+
+    @AfterEach
+    public void stopAuction() {
+        auction.stop();
+    }
+
+    @AfterEach
+    public void stopApplication() {
+        application.stop();
+    }
+}
