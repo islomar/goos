2c6a0decbf9c7e5fd30ac355941cb6a8557ad6f9
Isidro López <islomar@gmail.com>
2018-01-30 17:23:03 +0100

    Create the AuctionSniperDriver

---
+++ src/test/java/com/goos/auctionsniper/AuctionSniperDriver.java
@@ -0,0 +1,22 @@
+package com.goos.auctionsniper;
+
+
+import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JFrameDriver;
+import com.objogate.wl.swing.driver.JTableDriver;
+import com.objogate.wl.swing.gesture.GesturePerformer;
+
+public class AuctionSniperDriver extends JFrameDriver {
+
+    public AuctionSniperDriver(int timeoutMillis) {
+        super(new GesturePerformer(),
+                JFrameDriver.topLevelFrame(
+                        named(MainWindow.MAIN_WINDOW_NAME),
+                        showingOnScreen()),
+                new AWTEventQueueProber(timeoutMillis, 100));
+    }
+
+    public void showsSniperStatus(String statusText) {
+        new JTableDriver(this, named(Main.SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
+    }
+}
